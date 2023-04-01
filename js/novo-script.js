d607e6581a17845bc5c3998855542b49d7a6380b
perkles <camilaa.rodriggues17@gmail.com>
2023-04-01 09:18:48 -0300

    Finalizado

---
+++ js/novo-script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const mario = document.querySelector('.mario');
+    const pipe = document.querySelector('.pipe');
+
+    const jump = () => {
+        mario.classList.add('jump');
+
+        setTimeout(() =>{
+
+            mario.classList.remove('jump');
+
+            const pipePosition = pipe.offsetLeft;
+            const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+            if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+              pipe.style.animation = 'none';
+              pipe.style.left = `${pipePosition}px`;
+
+              mario.style.animation = 'none';
+              mario.style.bottom = `${marioPosition}px`;
+
+              mario.src = '/images/game-over.png';
+
+            }
+
+        }, 500);
+
+    }
+
+    document.addEventListener('keydown', jump);
+
+});
